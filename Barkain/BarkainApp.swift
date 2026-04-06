84bfdc8e691fd4576a4d76efb8ba8f27c64812cd
Mike <michaelolatunji23@icloud.com>
2026-04-06 10:45:51 -0400

    Initial Commit

---
+++ Barkain/BarkainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarkainApp.swift
+//  Barkain
+//
+//  Created by Michael Olatunji on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BarkainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
